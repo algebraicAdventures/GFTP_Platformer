e9e889142da3fb2fc809959510405516a3c098e9
Anthony Licata <tamamaroro@gmail.com>
2018-02-14 17:48:05 -0500

    Unity Project: - Standard assets - Test Visuals - Unfinished player, enemy, pickup scripts - Custom tile palatte

---
+++ GFTP_Platformer/Assets/Scripts/Pickup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public PickupType type = PickupType.coin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name.Contains("Player"))
+        {
+            switch (type)
+            {
+                case PickupType.coin:
+                    collision.GetComponent<Player>().incrementScore(1);
+                    break;
+                case PickupType.powerup:
+                    collision.GetComponent<Player>().powerup(5f);
+                    break;
+            }
+        }
+    }
+
+    public enum PickupType { coin, powerup };
+
+}
---
+++ GFTP_Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private int score = 0;
+    private bool canBeDamaged = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    // Increases score by amount
+    // returns current score
+    public int incrementScore(int amount)
+    {
+        return score += amount;
+    }
+
+    // Starts powerup effect for time amount of time
+    public void powerup(float time)
+    {
+        while(time > 0)
+        {
+            canBeDamaged = false;
+            time -= Time.deltaTime;
+        }
+        canBeDamaged = true;
+    }
+
+    // kills the player
+    public void die()
+    {
+
+    }
+}
